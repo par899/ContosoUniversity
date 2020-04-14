39eb1e53bdadb07b2358d27164f1ff45b45c4bf5
par899 <pardeep14kaur@gmail.com>
2020-04-13 21:30:29 -0400

    Add project files.

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
